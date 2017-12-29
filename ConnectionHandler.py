c81be61d2f10b5a3262421bea3cf19bb758c16a0
amahlaka <arttu.mahlakaarto@gmail.com>
2017-12-29 09:44:40 +0200

    asd

--- ConnectionHandler.py
+++ ConnectionHandler.py
@@ -58,7 +58,7 @@ def MainLoop():
     global CC_MESSAGE
     while True:
         if (CC_CONNECTED is True):
-            CC_MESSAGE = CC_MESSAGE+CC_SOCK.recv(1024)
+            CC_MESSAGE = CC_SOCK.recv(1024)
             CC_MES = CC_MESSAGE
             temp = string.split(CC_MESSAGE, "\n")
             CC_ARRAY = temp.pop()
