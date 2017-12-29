4cd380b6d06125316649281cc4b97e077b1e2fe5
amahlaka <arttu.mahlakaarto@gmail.com>
2017-12-29 08:46:44 +0200

    rewrite

--- ConnectionHandler.py
+++ ConnectionHandler.py
@@ -22,74 +22,40 @@ C1_BUFFER = ""
 CC_CONNECTED = False
 C1_CONNECTED = False
 C1_LISTEN = True
+CC_AUTH = False
 
 
-def ConnectToCC():
-    """Handle Connection to C&C Server."""
-    global CC_CONNECTED
+def SendMessage(sock, message):
+    """Send message to the socket."""
+    print("Sending message" + message)
+    print("\n " + message.encode())
+    sock.send(message.encode())
+
+
+def ConnectCC():
+    """Connect to C&C server."""
+    print("***CONNECTING TO C&C SERVER***")
     global CC_SOCK
-
-
-print("**** Connecting to: " + CC_HOST + ":" + str(CC_PORT) + " ****")
-CC_SOCK.connect((CC_HOST, CC_PORT))
-CC_SOCK.setblocking(False)
-CC_SOCK.send("USER %s %s bla :%s\r\n" % (CC_NAME, CC_NAME, "botteri"))
-#tmps = "USER " + CC_NAME + " " + CC_NAME + " " + CC_NAME + " :bro\n"
-#CC_SOCK.send(tmps)
-time.sleep(10)
-CC_SOCK.send("NICK " + CC_NAME + "\n")
-time.sleep(10)
-CC_SOCK.send("JOIN " + CC_CHAN + "\n")
-time.sleep(4)
-CC_SOCK.send("PRIVMSG " + CC_OWNR + " " + CC_NAME + " ALIVE\n")
-CC_SOCK.send("PRIVMSG " + CC_CHAN + " " + CC_NAME + " ALIVE\n")
-print("DONE\n")
-CC_CONNECTED = True
-
-
-def ConnectToC1():
-    """Handle Connection to Target Server."""
-    print("**** Connecting to: " + C1_HOST+":"+str(C1_PORT) + " ****")
-    C1_SOCK.connect((C1_HOST, C1_PORT))
-    time.sleep(10)
-    C1_SOCK.setblocking(False)
-    tmps = "USER " + C1_NAME + " " + C1_NAME + " " + C1_NAME + " :bro\r\n"
-    print(tmps)
-    C1_SOCK.send(tmps)
-    time.sleep(5)
-    C1_SOCK.send("NICK " + C1_NAME + "\r\n")
-    time.sleep(5)
-    C1_SOCK.send("JOIN " + C1_CHAN + "\r\n")
-    time.sleep(3)
-    CC_SOCK.send("PRIVMSG " + C1_CHAN + " " + C1_NAME + " ALIVE\n")
-    C1_CONNECTED = True
-    print("DONE\n")
-
-
-def CMD_WHOIS(USER):
-    print("\n REQUESTING WHOIS FOR USER: " + USER + " \n")
-    if(C1_CONNECTED):
-        C1_SOCK.send("WHOIS " + USER)
-        print("WHOIS command sent")
+    global CC_CONNECTED
+    CC_STAT = CC_SOCK.connect_ex((CC_HOST, CC_PORT))
+    if(CC_STAT is 0):
+        print("CONNECTED")
+        CC_CONNECTED = True
     else:
-        print("Not Connected to target server")
+        print("ERROR: " + CC_STAT)
+        exit()
+    print("DONE")
 
 
-ConnectToCC()
-#if(C1_CONNECTED is False):
-#    ConnectToC1()
-
-while True:
-    time.sleep(3)
-    print(".")
-    if(CC_CONNECTED is True):
-
-        CC_BUFFER = CC_SOCK.recv(1024)
-        print(CC_BUFFER)
-        temp = string.split(CC_BUFFER, "\n")
-        tmpb = temp.pop()
-        print(tmpb)
-        if("451" in tmpb):
-            CC_SOCK.send("USER %s %s bla :%s\r\n" % (CC_NAME, CC_NAME, CC_NAME))
-    #C1_BUFFER = C1_SOCK.recv(1024)
-    #print(C1_BUFFER)
+def MainLoop():
+    """Main Loop."""
+    global CC_SOCK
+    global CC_CONNECTED
+    global CC_AUTH
+    if (CC_CONNECTED is True):
+        if(CC_AUTH is False):
+            message = "NICK %S\r\n" + CC_NAME
+            SendMessage(CC_SOCK, message)
+            CC_AUTH = True
+        CC_MESSAGE = CC_SOCK.recv(1024)
+        print(CC_MESSAGE)
